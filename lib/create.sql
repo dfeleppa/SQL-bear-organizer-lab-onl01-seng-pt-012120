c4bbc7904f0752cc05693f8afc883e47e1c34baf
Daniel J Feleppa <dfeleppa@gmail.com>
2020-04-01 13:02:10 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,10 @@
+
 CREATE TABLE bears (
-  id INTEGER,
-  name TEXT,
-  age INTEGER,
-  gender TEXT,
-  color TEXT,
-  temperament TEXT,
-  alive INTEGER,
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    gender CHAR(1),
+    color TEXT,
+    temperament TEXT,
+    alive BOOLEAN
 );
