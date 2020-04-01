c4ed60fb901aa4597aa423b6618d00244321ece0
Daniel J Feleppa <dfeleppa@gmail.com>
2020-04-01 13:07:08 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT *"
+  "SELECT name, age FROM bears WHERE gender"
 end
 SELECT [names of columns we are going to select] FROM [table we are selecting from];
 
